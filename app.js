52af792f5194a342e954daacc89e53c064f750a9
ABDDOST <doston.yuldashev.88@gmail.com>
2025-01-22 20:48:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const app = express();
+const targetBaseUrl = "https://2960-84-54-82-215.ngrok-free.app/v1/email/nylas/webhook";
+
+// Disable SSL validation for development (remove in production)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios({
+            method: "GET",
+            url: targetBaseUrl,
+            params: req.query,
+        });
+        console.log(response.data, "response.data");
+
+        res.status(response.status).send(response.data);
+    } catch (error) {
+        console.error("Error forwarding request:", error.message);
+
+        const status = error.response ? error.response.status : 500;
+        const data = error.response ? error.response.data : "Error forwarding request";
+
+        res.status(status).send(data);
+    }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body, "req.body");
+
+    const response = await axios.post(targetBaseUrl,req.body);
+    console.log(response.data, "response.data");
+
+    res.sendStatus(response.status)
+  } catch (error) {
+    console.error("Error forwarding request:", error.message);
+
+    const status = error.response ? error.response.status : 500;
+    const data = error.response ? error.response.data : "Error forwarding request";
+    console.log(data, "data");
+
+    res.sendStatus(status)
+  }
+});
+
+app.listen(8087, () => {
+  console.log("Started server on port 8087");
+});
